d68ba9bb47dc1a7812121fd0e28ee0f209e95fd2
quocnghia32 <88430940+quocnghia32@users.noreply.github.com>
2024-07-01 22:44:04 +0700

    Change Fragment in Main Activity

--- app/src/main/java/com/example/travelbooking/MainActivity.java
+++ app/src/main/java/com/example/travelbooking/MainActivity.java
@@ -8,17 +8,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.travelbooking.databinding.ActivityMainBinding;
+import androidx.fragment.app.Fragment;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.HomeB) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.BookingB) {
+                replaceFragment(new BookingFragment());
+            } else if (item.getItemId() == R.id.NotificationB) {
+                replaceFragment(new NotificationFragment());
+            } else if (item.getItemId() == R.id.AccountB) {
+                replaceFragment(new AccountFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
     }
 }
